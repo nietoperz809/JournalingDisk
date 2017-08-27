8d4f57502acb8eee3ba793d1c9bc8db3576f45ac
Administrator <pbull809@gmail.com>
2017-08-27 12:23:25 +0200

    added test

--- Test/JournalingDiskTest.java
+++ Test/JournalingDiskTest.java
@@ -1,48 +1,49 @@
-package main;
 
 import jdisk.JournalingDisk;
+import org.junit.Test;
 
-import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
-public class Tester
+public class JournalingDiskTest
 {
-    public static void main (String[] args)
+    @Test
+    public void test1 () throws Exception
     {
         String hello = "hello world";
         String doof  = "fuck off du";
         String dumm  = "1234567890a";
         JournalingDisk disk = new JournalingDisk(100, hello.length());
+        byte[] empty = disk.read(0);
 
         disk.beginTransaction();
         disk.write(0, hello.getBytes());
         disk.endTransaction();
         byte[]cc = disk.read(0);
-        System.out.println(Arrays.toString(cc));
+        assertEquals (hello, new String(cc));
 
         disk.beginTransaction();
         disk.write(0, doof.getBytes());
         disk.endTransaction();
         cc = disk.read(0);
-        System.out.println(Arrays.toString(cc));
+        assertEquals (doof, new String(cc));
 
         disk.beginTransaction();
         disk.write(0, dumm.getBytes());
         disk.endTransaction();
         cc = disk.read(0);
-        System.out.println(Arrays.toString(cc));
-
-        System.out.println("-----------------------------------");
+        assertEquals (dumm, new String(cc));
 
         disk.rollback();
         cc = disk.read(0);
-        System.out.println(Arrays.toString(cc));
+        assertEquals (doof, new String(cc));
 
         disk.rollback();
         cc = disk.read(0);
-        System.out.println(Arrays.toString(cc));
+        assertEquals (hello, new String(cc));
 
         disk.rollback();
         cc = disk.read(0);
-        System.out.println(Arrays.toString(cc));
+        assertArrayEquals (empty, cc);
     }
 }
